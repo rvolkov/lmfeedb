c399e9ae091800a78d89ed63c5044c79896e1bbd
rvolkov <rvolkov@gmail.com>
2017-03-20 11:39:35 +1100

    change timeout to string

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app.config.update(SEND_FILE_MAX_AGE_DEFAULT=0)
 # any strange string here - this is key to generate tokens
 app.config.update(SECRET_KEY='sadfwerusafgr-sesfgfgcret-jdddh')
 app.config.update(BASEDIR=os.path.abspath(os.path.dirname(__file__)))
-app.config.update(JWT_EXPIRATION_DELTA=129600)
+app.config.update(JWT_EXPIRATION_DELTA='129600')
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 from app import views
